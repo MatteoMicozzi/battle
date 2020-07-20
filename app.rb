294310327dfb873a12a17682a2efdcc4c719a1dc
DillonBarker <barker.dill98@gmail.com>
2020-07-20 16:10:57 +0100

    adding extra ports and using shotgun

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'shotgun'
+
+set :session_secret, 'super secret'
 
 get '/' do
   "Hello World"
+end
+
+get '/secret' do
+  "Secret Message!?!"
+end
+
+get '/dill' do
+  "Hi I am dill"
 end

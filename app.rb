6e4e0f598a8ca0aa83a37011f9ac2fa02e68d1cd
Matteo Micozzi <micozzimatteo@yahoo.it>
2020-07-20 16:42:05 +0100

    updating app with /cat

--- app.rb
+++ app.rb
@@ -14,3 +14,13 @@ end
 get '/dill' do
   "Hi I am dill"
 end
+
+get '/matt' do
+  "Hi I'm Matteo"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
